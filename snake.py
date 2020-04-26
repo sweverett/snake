bcd2ae0e8d904fb6063da7206d2ddd144b5506bc
Spencer <spencerweverett@gmail.com>
2020-04-25 22:42:16 -0700

    Created the main file

---
+++ snake.py
@@ -0,0 +1,18 @@
+import pygame
+
+class Snake(object):
+    pass
+
+class Square(object):
+    pass
+
+def fund():
+    pass
+
+def main():
+    print('I"m in snake.py')
+    return
+
+if __name__ == '__main__':
+    main()
+
